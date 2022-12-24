a3a62a729662af372177616bed41579e9242015a
Trevor Brooks <trevorism@gmail.com>
2022-12-24 12:01:46 -0500

    update filter

--- src/acceptance/groovy/com/trevorism/gcloud/FilterOperationsDefinitions.groovy
+++ src/acceptance/groovy/com/trevorism/gcloud/FilterOperationsDefinitions.groovy
@@ -28,7 +28,7 @@ Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").create()
 
 When(/a filter request with date before {int} seconds from now is requested/) { Integer int1 ->
     String dateValue = Instant.now().minusSeconds(int1).toString()
-    ComplexFilter filter = new ComplexFilter(lookup: "datastore:arbitrary", simpleFilters: [new SimpleFilter(field: "date", operator: "<", value: dateValue)])
+    ComplexFilter filter = new ComplexFilter(lookup: "datastore:arbitrary", simpleFilters: [new SimpleFilter(field: "date", operator: "<", type: "date", value: dateValue)])
     String json = gson.toJson(filter)
     String jsonList = secureHttpClient.post("https://data.trevorism.com/filter", json)
     list = gson.fromJson(jsonList, new TypeToken<List<Arbitrary>>() {}.getType())
