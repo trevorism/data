791743f605d2667701db0f92e7daa291080a42e3
Trevor Brooks <trevorism@gmail.com>
2024-09-07 16:39:46 -0400

    Fix unit tests that contain the optional parameter

--- src/test/groovy/com/trevorism/gcloud/webapi/controller/ObjectControllerTest.groovy
+++ src/test/groovy/com/trevorism/gcloud/webapi/controller/ObjectControllerTest.groovy
@@ -9,30 +9,30 @@ class ObjectControllerTest {
     @Test
     void testRead() {
         ObjectController oc = new ObjectController([get: { url -> return "{\"id\":\"12341\",\"name\":\"testName\"}"}] as SecureHttpClient)
-        assert oc.read("testpushbutton", "6318495402819584")
+        assert oc.read("testpushbutton", "6318495402819584", Optional.of("memory"))
     }
 
     @Test
     void testReadAll() {
         ObjectController oc = new ObjectController([get: { url -> return "[{\"id\":\"12341\",\"name\":\"testName\"}]"}] as SecureHttpClient)
-        assert oc.readAll("testpushbutton")
+        assert oc.readAll("testpushbutton", Optional.of("bigquery"))
     }
 
     @Test
     void testCreate() {
         ObjectController oc = new ObjectController([post: { url, obj -> return "{\"id\":\"12341\",\"name\":\"testName\"}"}] as SecureHttpClient)
-        assert oc.create("testpushbutton",[id:12341,name:"testName"])
+        assert oc.create("testpushbutton",[id:12341,name:"testName"], Optional.empty())
     }
 
     @Test
     void testUpdate() {
         ObjectController oc = new ObjectController([put: { url, obj -> return "{\"id\":\"12341\",\"name\":\"testName\"}"}] as SecureHttpClient)
-        assert oc.update("testpushbutton",12341,[id:12341,name:"testName"])
+        assert oc.update("testpushbutton",12341,[id:12341,name:"testName"], Optional.empty())
     }
 
     @Test
     void testDelete() {
         ObjectController oc = new ObjectController([delete: { url -> return "{\"id\":\"12341\",\"name\":\"testName\"}"}] as SecureHttpClient)
-        assert oc.delete("testpushbutton", 6318495402819584)
+        assert oc.delete("testpushbutton", 6318495402819584, Optional.empty())
     }
 }
